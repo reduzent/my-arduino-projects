55fadd6205bdd6f40378442e44ab231dae5cc5bd
Roman Haefeli <reduzent@gmail.com>
2026-09-19 20:24:13 +0200

    fix no of closing parentheses

--- garten_motor_vernissage/garten_motor_vernissage.ino
+++ garten_motor_vernissage/garten_motor_vernissage.ino
@@ -204,7 +204,7 @@ void ratsche() {
 
 void flasche() {
   rampUp(FLASCHE, 500);
-  play(FLASCHE, 255, (8000) + (random(3) * 8000))); // 8, 16, or 24 s
+  play(FLASCHE, 255, (8000) + (random(3) * 8000)); // 8, 16, or 24 s
   rampDown(FLASCHE, 500);
 }
 
